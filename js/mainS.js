7a69c9b5ff7144bb84dfee9cdf9bd16119904ee8
snehasisb <104086465+snehasisb@users.noreply.github.com>
2022-04-29 23:02:30 +0530

    Add files via upload

--- js/mainS.js
+++ js/mainS.js
@@ -12,3 +12,33 @@ const tabs = document.querySelectorAll('[data-tab-value]')
                 target.classList.add('active');
             })
         })
+
+function getBrowserSize(){
+            var w, h;
+     
+              if(typeof window.innerWidth != 'undefined')
+              {
+               w = window.innerWidth; //other browsers
+               h = window.innerHeight;
+              } 
+              else if(typeof document.documentElement != 'undefined' && typeof      document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) 
+              {
+               w =  document.documentElement.clientWidth; //IE
+               h = document.documentElement.clientHeight;
+              }
+              else{
+               w = document.body.clientWidth; //IE
+               h = document.body.clientHeight;
+              }
+            return {'width':w, 'height': h};
+     }
+     
+if(parseInt(getBrowserSize().width) < 800){ //small display
+
+      var tempS = document.getElementById("profileDiv").innerHTML;
+      document.getElementById("profileDivHome").innerHTML = tempS;
+      document.getElementById("profileDiv").innerHTML = "";
+      //document.getElementById("separateMenu").innerHTML = "<br/>";
+      
+      
+}
